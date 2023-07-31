c69ef6bd55a7fe5c42e76084965d522cd25d0b45
sajeevanh2r <136316660+sajeevanh2r@users.noreply.github.com>
2023-07-31 21:51:32 +0530

    JAVA LAB
    JAVA TUTORIALS

---
+++ JAVA 28504/ColorChanging/src/main/java/com/mycompany/colorchanging/ColorChanging.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.colorchanging;
+public class ColorChanging 
+{
+
+    public static void main(String[] args) 
+    {
+        ColorDemo c=new ColorDemo();
+        c.show();
+    }
+}
